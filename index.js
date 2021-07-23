eea839dbf950a61aaa27ca887da2728b29f5a6ef
Leonardo Guzmán <lguzs96@mail.com>
2021-07-22 23:08:10 -0500

    I'm in the process of learning js

---
+++ index.js
@@ -0,0 +1,74 @@
+/* console.log('fff');
+
+
+var edad = 3;
+console.log(edad);
+edad =34;
+ */
+/* let saludo= prompt('Hola ¿cuál es tu nombre?');
+alert('Hola '+ saludo); */
+
+/* Concatenando texto */
+/* let saludo ='Hola, Leonardo ',
+	pregunta ='¿Cómo estás?';
+	document.write(saludo + pregunta); */
+/* Concatenando dos numeros */
+/* let numero1 = '3',
+	numero2 = 7;
+	frase= ''+ numero1+numero2;
+	document.write(frase); */
+
+	/* let frase1 = 'El viento se llevó todo, ', // control + alt + } para esas comillas
+		frase = `${frase1} soy Leonardo`;
+	
+	document.write(frase); */
+
+	/* un booleano */
+/* let edad =27;
+if (edad>=18 ) {
+console.log ('Aceptado');
+} else {
+	console.log('Rehazado');
+} */
+
+/* esta es una función */
+/* function mostrarMensaje (){
+	document.write('Hola bb');
+}
+mostrarMensaje (); */
+
+//Funcion de cuadrado
+/* let number;
+function square(number) {
+	return number * number;
+  }
+  document.write(square (5)); */
+
+//funcion con texto
+/* function saludo () {
+	document.write('Binevenido a mi página');
+}
+saludo (); */
+
+// función anónima
+let saludo = function () {
+	document.write('Hola esta es una funcion anonima');
+}
+saludo ();
+
+/* let numero1 =10, numero2=2;
+ 
+ resultado = -numero1;
+document.write (resultado) */
+
+var a = 5, b = 10, c = 15;
+document.write((3 + b)*c/a*2);
+document.write(((a+b*c)/5)*2);
+
+
+
+/* function suma (numero1, numero2) {
+	return numero1 + numero2;
+}
+
+console.log(suma(4,8)); */
---
+++ switch/index.js
@@ -0,0 +1,31 @@
+//programa que te devuelva un personaje de breaking Bad dependiendo el número que ingreses
+
+let personaje = 5;
+let mensaje;
+
+switch (personaje) {
+    case (personaje=1): 
+    document.write(mensaje= 'Jesse Pinkman');
+    break;
+    case (personaje=2): 
+    document.write(mensaje= 'Gus Fring');
+    break;
+    case (personaje=3): 
+    document.write(mensaje= 'Walter White');
+    break;
+    case (personaje=4): 
+    document.write(mensaje= 'Saul Goodman');
+    break;
+    case (personaje=5): 
+    document.write(mensaje= 'Skyler');
+    break;
+    case (personaje=6): 
+    document.write(mensaje= 'Tuco Salamanca');
+    break;
+    default:
+        document.write(mensaje= 'Eso no vale xD');
+}
+
+// let edad = 67;
+// let aclaracion = (edad>30) ? 'se puede vacunar' : 'No se puede vacunar';
+// console.log(aclaracion)
